dbcc241cb631bf3fdbf1ab822ffa2aa787c5d150
haju <haju0321@gmail.com>
2021-12-09 09:12:07 +0900

    Initial Commit

---
+++ YamyRecipes/YamyRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YamyRecipesApp.swift
+//  YamyRecipes
+//
+//  Created by haju Kim on 2021/12/09.
+//
+
+import SwiftUI
+
+@main
+struct YamyRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
